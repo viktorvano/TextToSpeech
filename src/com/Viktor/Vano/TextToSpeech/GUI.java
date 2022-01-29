aaebf4e34ed48ba30ba2a92cdb982fefa4a07ddc
Viktor Vaňo <viktorvano@gmail.com>
2022-01-29 13:07:22 +0100

    Adding a screenshot

--- src/com/Viktor/Vano/TextToSpeech/GUI.java
+++ src/com/Viktor/Vano/TextToSpeech/GUI.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 import static com.Viktor.Vano.TextToSpeech.FileManager.*;
 
 public class GUI extends Application {
-    private final String verion = "20220117";
+    private final String version = "20220117";
     private int port = 7775;
     private final int width = 400;
     private final int height = 120;
@@ -29,7 +29,7 @@ public class GUI extends Application {
 
         Scene scene = new Scene(pane, width, height);
 
-        stage.setTitle("Text To Speech " + verion);
+        stage.setTitle("Text To Speech " + version);
         stage.setScene(scene);
         stage.show();
         stage.setMinWidth(stage.getWidth());
